29e5780cf3a04b2950454ac0a80384647dc01034
SemperPhoenix <semperphoenixaz@gmail.com>
2017-08-03 17:14:11 -0700

    adding local files to GitHub

---
+++ 05 Variables/variables-lists.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# variables.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    x = [1, 2, 3]
+    x.append(5)
+    x.insert(2, 7)
+    print(type(x), x)
+
+if __name__ == "__main__": main()
